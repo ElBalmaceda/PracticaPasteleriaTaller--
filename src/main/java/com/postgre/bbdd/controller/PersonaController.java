28c50218e73556e5c8246e2df42a3d5cafb676bb
Balmaceda <150931922+ElBalmaceda@users.noreply.github.com>
2024-12-17 11:58:17 -0300

    Crud completo

--- src/main/java/com/postgre/bbdd/controller/PersonaController.java
+++ src/main/java/com/postgre/bbdd/controller/PersonaController.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.postgre.bbdd.entity.Pastel;
@@ -37,7 +39,7 @@ public class PersonaController
 	public String savePastel(Pastel pastel) 
 	{
 		pastelService.save(pastel);
-		return "redirect:/pasteles-list";
+		return "redirect:/pasteles";
 	}
 	
 	@GetMapping("/editar/{id}")
@@ -46,12 +48,23 @@ public class PersonaController
 		model.addAttribute("updatePastel",pastelService.findById(id));
 		return "pastel-update";
 	}
+	@PostMapping("/{id}")
+	public String updatePastel(@PathVariable("id") Long id,
+			@ModelAttribute("updatePastel") Pastel pastel, Model model) 
+	{
+		Pastel p=pastelService.findById(id);
+		p.setId(pastel.getId());
+		p.setNombre(pastel.getNombre());
+		p.setSabor(pastel.getSabor());
+		pastelService.save(p);
+		return "redirect:/pasteles";
+	}
 	
 	@GetMapping("/eliminar/{id}")
 	public String eliminarPastel(@PathVariable("id") Long id) 
 	{
 		pastelService.deleteById(id);
-		return "redirect:/pasteles-list";
+		return "redirect:/pasteles";
 	}
 	
 	
